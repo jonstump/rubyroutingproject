665b6fb90b5eac82465cb01246c48add531a32a5
Jon Stump <jmstump@gmail.com>
2021-03-07 10:33:48 -0800

    add notes for methods for word and definitions

--- lib/definition.rb
+++ lib/definition.rb
@@ -2,9 +2,11 @@ class Definitions
   attr_reader :definition_id
   attr_accessor :definition, :word_id, :user
 
+  # creats an empty hash for definitions and a definition id counter
   @@definitions = {}
   @@definition_id = 0
 
+  #instantiates a definitions hash
   def initialize(attributes)
     @user = attributes.fetch(:user)
     @definition = attributes.fetch(:definition)
@@ -12,31 +14,38 @@ class Definitions
     @definition_id = attributes.fetch(:definition_id) || @@definition_id += 1
   end
 
+  #finds all values in definitions
   def self.all
     @@definitions.values
   end
 
+  #compares definitions based on the definition itself and the word id
   def ==(compare_def)
     (self.definition() == compare_def.definition()) && (self.word_id() == compare_def.word_id())
   end
 
+  #saves a definition in the definitions hash
   def save
     @@definitions[self.definition_id] = Definitions.new({:definition => self.definition, :user => self.user, :definition_id => self.definition_id, :word_id => self.word_id})
   end
 
+  #clears the definitions hash of all values and resets the id counter
   def self.clear
     @@definitions = {}
     @@definition_id = 0
   end
 
+  #finds a definition by the id
   def self.find(definition_id)
     @@definitions[definition_id]
   end
 
+  #deletes a definition by the id
   def delete
     @@definitions.delete(self.definition_id)
   end
 
+  #finds definitions by the wrods id and puts all of them into a new array. 
   def self.find_by_word(words_id)
     definitions = []
     @@definitions.values.each do |definition|
--- lib/word.rb
+++ lib/word.rb
@@ -2,44 +2,54 @@ class Words
   attr_accessor :user_word, :user 
   attr_reader :id
 
+  #create an empty hash for words and a counter for ID numbers
   @@words = {}
   @@total_words = 0
 
+  # Instantiates a word hash
   def initialize(attributes)
     @user = attributes.fetch(:user)
     @user_word = attributes.fetch(:user_word)
     @id = attributes.fetch(:id) || @@total_words += 1
   end
 
+  #brings up all values in the word hash
   def self.all
     @@words.values
   end
 
+  #compares words to see if they are the same
   def ==(compare_words)
     self.user_word = compare_words.user_word
   end
 
+  #saves the words based on an id
   def save
     @@words[self.id] = Words.new({:user_word => self.user_word, :user => self.user, :id => self.id})
   end
 
+  #clears the word hash of all values
   def self.clear
     @@words = {}
     @@total_words = 0
   end
 
+  #finds word based on id
   def self.find(id)
     @@words[id]
   end
 
+  #deletes a word based on id
   def delete
     @@words.delete(self.id)
   end
 
+  #finds defintions created by the definition class by word id
   def definitions
     Definitions.find_by_word(self.id)
   end
 
+  #sords words alphabetically
   def self.word_sort()
     @@words.values.sort_by { |word| [word.user_word]}
   end
